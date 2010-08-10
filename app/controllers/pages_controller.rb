a47d499e7155738a849de7c4cc169111d28958ad
pierreva <pierre.vaudrey@gmail.com>
2010-08-10 15:30:49 +0200

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,18 @@
 class PagesController < ApplicationController
   def home
-      @title = "Home"
-    end
+    @title = "Home"
+  end
 
-    def contact
-      @title = "Contact"
-    end
+  def contact
+    @title = "Contact"
+  end
 
-    def about
-      @title = "About"
-    end
-  
-  
+  def about
+    @title = "About"
+  end
+  def help
+    @title = "Help"
+  end
+
 
 end
